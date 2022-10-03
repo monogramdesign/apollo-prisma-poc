9a5e9a018b87328d318b3c0c31352c62cc7d4d6f
Chalo Salvador <chalo@monogram.io>
2022-10-03 12:40:26 -0500

    Fix addProduct - Prettier formatting

--- src/schema.ts
+++ src/schema.ts
@@ -1,17 +1,18 @@
-import { gql } from 'apollo-server'
-import { DateTimeResolver } from 'graphql-scalars'
-import { context, Context } from './context'
+import { Category } from "@prisma/client";
+import { gql } from "apollo-server";
+import { DateTimeResolver } from "graphql-scalars";
+import { context, Context } from "./context";
 
 export const typeDefs = gql`
   type Product {
-    id:        ID!
+    id: ID!
     createdAt: DateTime
     updatedAt: DateTime
-    name:     String!
+    name: String!
     published: Boolean!
-    price:    Float!
-    category:    Category!
-    orders:  [Order]
+    price: Float!
+    category: Category!
+    orders: [Order]
   }
 
   type Category {
@@ -38,17 +39,21 @@ export const typeDefs = gql`
     allProducts: [Product!]!
     productsByCategory(categoryId: Int!): [Product]
     productById(id: Int): Product
-    
-    allUsers: [User!]!
-    
-    allCategories: [Category!]!
-    
-    allOrders: [Order!]!
 
+    allUsers: [User!]!
+
+    allCategories: [Category!]!
+
+    allOrders: [Order!]!
   }
 
   type Mutation {
-    addProduct(name: String!, price: Float!, published: Boolean, category: CategoryUniqueInput!): Product
+    addProduct(
+      name: String!
+      price: Float!
+      published: Boolean
+      category: CategoryUniqueInput!
+    ): Product
   }
 
   input ProductOrderByPriceAtInput {
@@ -66,115 +71,51 @@ export const typeDefs = gql`
   }
 
   scalar DateTime
-`
+`;
 
 export const resolvers = {
   Query: {
     allProducts: (_parent, _args, context: Context) => {
-      return context.prisma.product.findMany()
+      return context.prisma.product.findMany();
     },
     productById: (_parent, args: { id: number }, context: Context) => {
       return context.prisma.product.findUnique({
         where: { id: args.id || undefined },
-      })
+      });
     },
-    productsByCategory: (_parent, args: { categoryId: number }, context: Context) => {
+    productsByCategory: (
+      _parent,
+      args: { categoryId: number },
+      context: Context
+    ) => {
       return context.prisma.product.findMany({
         where: {
-          categoryId: args.categoryId
-        }
-      })
+          categoryId: args.categoryId,
+        },
+      });
     },
   },
   Mutation: {
     addProduct: (
-      _parent, 
-      args: {data: {name: string, price: number, published: boolean, categoryId: number}}
-      ) => {
-        return context.prisma.product.create({
-          data:{
-            name: args.data.name,
-            price: args.data.price,
-            published: args.data.published,
-            categoryId: args.data.categoryId,
-          }
-        })
+      _parent,
+      args: {
+        name: string;
+        price: number;
+        published: boolean;
+        category: Category;
       }
-  //   signupUser: (
-  //     _parent,
-  //     args: { data: UserCreateInput },
-  //     context: Context,
-  //   ) => {
-  //     const postData = args.data.posts?.map((post) => {
-  //       return { title: post.title, content: post.content || undefined }
-  //     })
-
-  //     return context.prisma.user.create({
-  //       data: {
-  //         name: args.data.name,
-  //         email: args.data.email,
-  //         posts: {
-  //           create: postData,
-  //         },
-  //       },
-  //     })
-  //   },
-  //   createDraft: (
-  //     _parent,
-  //     args: { data: PostCreateInput; authorEmail: string },
-  //     context: Context,
-  //   ) => {
-  //     return context.prisma.post.create({
-  //       data: {
-  //         title: args.data.title,
-  //         content: args.data.content,
-  //         author: {
-  //           connect: { email: args.authorEmail },
-  //         },
-  //       },
-  //     })
-  //   },
-  //   togglePublishPost: async (
-  //     _parent,
-  //     args: { id: number },
-  //     context: Context,
-  //   ) => {
-  //     try {
-  //       const post = await context.prisma.post.findUnique({
-  //         where: { id: args.id || undefined },
-  //         select: {
-  //           published: true,
-  //         },
-  //       })
-
-  //       return context.prisma.post.update({
-  //         where: { id: args.id || undefined },
-  //         data: { published: !post?.published },
-  //       })
-  //     } catch (error) {
-  //       throw new Error(
-  //         `Post with ID ${args.id} does not exist in the database.`,
-  //       )
-  //     }
-  //   },
-  //   incrementPostViewCount: (
-  //     _parent,
-  //     args: { id: number },
-  //     context: Context,
-  //   ) => {
-  //     return context.prisma.post.update({
-  //       where: { id: args.id || undefined },
-  //       data: {
-  //         viewCount: {
-  //           increment: 1,
-  //         },
-  //       },
-  //     })
-  //   },
-  //   deletePost: (_parent, args: { id: number }, context: Context) => {
-  //     return context.prisma.post.delete({
-  //       where: { id: args.id },
-  //     })
+    ) => {
+      return context.prisma.product.create({
+        data: {
+          name: args.name,
+          price: args.price,
+          published: args.published,
+          category: {
+            connect: { id: args.category.id },
+          },
+        },
+      });
+    },
   },
   DateTime: DateTimeResolver,
   Product: {
@@ -183,7 +124,7 @@ export const resolvers = {
         .findUnique({
           where: { id: parent?.id },
         })
-        .category()
+        .category();
     },
   },
   Category: {
@@ -192,32 +133,7 @@ export const resolvers = {
         .findUnique({
           where: { id: parent?.id },
         })
-        .products()
+        .products();
     },
   },
-}
-
-// enum SortOrder {
-//   asc = 'asc',
-//   desc = 'desc',
-// }
-
-// interface PostOrderByUpdatedAtInput {
-//   updatedAt: SortOrder
-// }
-
-// interface UserUniqueInput {
-//   id?: number
-//   email?: string
-// }
-
-// interface PostCreateInput {
-//   title: string
-//   content?: string
-// }
-
-// interface UserCreateInput {
-//   email: string
-//   name?: string
-//   posts?: PostCreateInput[]
-// }
+};
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import { ApolloServer } from 'apollo-server'
 import { resolvers, typeDefs } from './schema'
 import { context } from './context'
 
-new ApolloServer({ resolvers, typeDefs, context: context, introspection: process.env.INTROSPECTION === 'true'
-}).listen(
-  { port: 4000 },
-).then(({url}) =>
-console.log(`
-🚀 Server ready at: ${url}. INTROSPECTION=${process.env.INTROSPECTION}`)
-)
+new ApolloServer({
+  resolvers,
+  typeDefs,
+  context: context,
+  introspection: process.env.INTROSPECTION === "true",
+})
+  .listen({ port: 4000 })
+  .then(({ url }) => {
+    `🚀 Server ready at: ${url}. INTROSPECTION=${process.env.INTROSPECTION}`;
+  });
